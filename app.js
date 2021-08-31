3eae268772de97feff9939eb3de002409dfb5294
vinayakg123 <88088473+vinayakg123@users.noreply.github.com>
2021-08-31 14:17:04 +0530

    Final structure

---
+++ app.js
@@ -0,0 +1,28 @@
+var btnTranslate = document.querySelector("#btn-translate");
+var txtInput = document.querySelector("#txt-input");
+var outputDiv = document.querySelector("#output");
+
+var serverURL="https://api.funtranslations.com/translate/minion.json"
+
+function getTranslationURL(text) {
+  return serverURL + "? " + "text= "+ text
+}
+
+function errorHandler(error) {
+  console.log("error occured ", error)
+  alert ("Something wrong with server: try again after sometime")
+}
+
+btnTranslate.addEventListener("click",function clickEventHandler () {
+
+var inputText = txtInput.value;
+
+fetch(getTranslationURL(inputText))
+.then(response => response.json())
+.then(json => {
+  var translatedText = json.contents.translated;
+  outputDiv.innerText = translatedText;
+})
+.catch(errorHandler)
+  
+})
